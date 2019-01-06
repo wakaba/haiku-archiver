20fd1bcfe006d2f9f8fc462277b9eb817d550734
Wakaba <wakaba@suikawiki.org>
2019-01-06 17:47:49 +0900

    empty script

---
+++ bin/har.pl
@@ -0,0 +1,34 @@
+use strict;
+use warnings;
+
+sub main ($) {
+  my $command = $_[0];
+  if ($command eq 'XXX') {
+    
+  } else {
+    die "Usage: har command\n";
+  }
+} # main
+
+my $Command = shift // '';
+main ($Command);
+
+=head1 LICENSE
+
+Copyright 2019 Wakaba <wakaba@suikawiki.org>.
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as
+published by the Free Software Foundation, either version 3 of the
+License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public
+License along with this program.  If not, see
+<https://www.gnu.org/licenses/>.
+
+=cut
